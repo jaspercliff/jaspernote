e788335a5c4965ec9fb313729bfb4940ce8f8cdf
jasper <2190320413@qq.com>
2025-03-27 11:58:00 +0800

    shell 参数传递

--- .vitepress/configs/sidebarShell.js
+++ .vitepress/configs/sidebarShell.js
@@ -5,7 +5,7 @@ export const sidebarShell = {
             items: [
                 { text: "index", link: "/os/linux/shell/index.md" },
                 { text: "流程控制", link: "/os/linux/shell/control.md" },
-                {text : "变量",link: "/os/linux/shell/变量.md" },
+                {text : "变量",link: "/os/linux/shell/variable.md" },
                 {text : "大括号扩展符{}",link: "/os/linux/shell/braceExpansion.md" },
             ],
         },
