8864aabacb5c9e72b1553b2ff9a3b63d9f03dcf3
jasper <2190320413@qq.com>
2025-05-09 18:00:41 +0800

    修复参数表格式并更新侧边栏，新增“参数传递”和“运算符”链接。

--- .vitepress/configs/sidebarShell.js
+++ .vitepress/configs/sidebarShell.js
@@ -4,8 +4,11 @@ export const sidebarShell = {
             text: "shell",
             items: [
                 { text: "index", link: "/os/linux/shell/index.md" },
-                { text: "流程控制", link: "/os/linux/shell/control.md" },
+                { text: "basic", link: "/os/linux/shell/basic.md" },
                 {text : "变量",link: "/os/linux/shell/variable.md" },
+                {text : "参数传递",link: "/os/linux/shell/param.md" },
+                {text : "运算符",link: "/os/linux/shell/operator.md" },
+                { text: "流程控制", link: "/os/linux/shell/control.md" },
                 {text : "大括号扩展符{}",link: "/os/linux/shell/braceExpansion.md" },
             ],
         },
